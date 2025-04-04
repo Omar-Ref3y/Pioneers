19a6e044fc3371b173ecfb59b7251a2b27d0e150
Omar Refay <omarrefay2004@gmail.com>
2025-04-04 16:49:43 +0200

    fix: update appwrite config for production

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,9 +1,10 @@
-import { Client, Databases } from 'appwrite';
+import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
-const databases = new Databases(client);
+export const account = new Account(client);
+export const databases = new Databases(client);
 
-export { client, databases };
+export default client;
